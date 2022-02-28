7ab7666200e573be6de20066cacff47797ae7091
baekdonggi <bakdonggi@naver.com>
2022-02-28 20:43:48 +0900

    이미지 업로드테스트

---
+++ team_first_project02/models/index.js
@@ -0,0 +1,50 @@
+const { sequelize } = require('./connection');
+const Product = require('./product');
+const ProductCategory = require('./productcategory');
+const Cart = require('./cart');
+const ProductReply = require('./productreply');
+const ProductImageFile = require('./productimagefile');
+const UserOrderDetail = require('./userorderdetail');
+const User = require('./user');
+const UserOrder = require('./userorder');
+
+const UserOrderRefund = require('./userorderrefund');
+
+const db = {};
+
+db.sequelize = sequelize;
+
+// model 생성
+db.Product = Product;
+db.ProductCategory = ProductCategory;
+db.Cart = Cart;
+db.ProductReply = ProductReply;
+db.ProductImageFile = ProductImageFile;
+db.UserOrderDetail = UserOrderDetail;
+db.User = User;
+db.UserOrder = UserOrder;
+db.UserOrderRefund = UserOrderRefund;
+
+// model init
+Product.init(sequelize);
+ProductCategory.init(sequelize);
+Cart.init(sequelize);
+ProductReply.init(sequelize);
+ProductImageFile.init(sequelize);
+UserOrderDetail.init(sequelize);
+User.init(sequelize);
+UserOrder.init(sequelize);
+UserOrderRefund.init(sequelize);
+
+// association(관계 생성)
+Product.associate(db);
+ProductCategory.associate(db);
+Cart.associate(db);
+ProductReply.associate(db);
+ProductImageFile.associate(db);
+UserOrderDetail.associate(db);
+User.associate(db);
+UserOrder.associate(db);
+UserOrderRefund.associate(db);
+
+module.exports = db;
---
+++ team_first_project02/models/user.js
@@ -0,0 +1,64 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      userId: {
+        type: Sequelize.STRING(30),
+        unique: true,
+      },
+      userPassword: {
+        type: Sequelize.STRING(217),
+      },
+      userName: {
+        type: Sequelize.STRING(50),
+      },
+      userPhone: {
+        type: Sequelize.STRING(20),
+      },
+      userEmail: {
+        type: Sequelize.STRING(50),
+      },
+      userNickname: {
+        type: Sequelize.STRING(20),
+        unique: true,
+      },
+      userProfile: {
+        type: Sequelize.STRING(100),
+      },
+      emailCheck: {
+        type: Sequelize.STRING(1),
+      },
+      userGender: {
+        type: Sequelize.INTEGER,
+      },
+      userRole: {
+        type: Sequelize.INTEGER,
+      },
+      userGrade: {
+        type: Sequelize.STRING(10),
+      },
+      userAddress1: {
+        type: Sequelize.STRING(20),
+      },
+      userAddress2: {
+        type: Sequelize.STRING(50),
+      },
+      userAddress3: {
+        type: Sequelize.STRING(50),
+      },
+    }, {
+      sequelize,
+      // tableName: 'tableName', // table명을 수동으로 생성 함
+      // freezeTableName: true, // true: table명의 복수형 변환을 막음
+      underscored: true, // true: underscored, false: camelCase
+      timestamps: true, // createAt, updatedAt
+      paranoid: true, // deletedAt
+    });
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.UserOrder, { foreignKey: 'userId', sourceKey: 'userId' });
+    // db.User.hasMany(db.UserOrder, { foreignKey: { name: 'userId' }, onDelete: 'SET NULL', as: 'userorder' });
+  }
+};
